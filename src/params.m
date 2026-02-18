105c042a23a74dfc30b785c7a968036d32b0f7b0
Shiva M <148998407+Shiva9641725@users.noreply.github.com>
2026-02-18 14:49:43 -0600

    parameter file

---
+++ src/params.m
@@ -0,0 +1,26 @@
+function p = params()
+
+p.T_vals = 325;
+
+p.sigma_max_AM = 750;
+p.sigma_max_MA = 750;
+
+p.e_trans = 0.05;
+p.E = 70000;
+
+p.Cf = 7.0;
+p.Cm = 7.0;
+
+p.Mf = 286; 
+p.Ms = 306;
+p.As = 310;
+p.Af = 335;
+
+p.Mp = 296;
+p.Ap = 322.5;
+
+p.kappa = 2.9444;
+p.dsigma = 1;
+p.zeroguard = 1e-9;
+
+end
